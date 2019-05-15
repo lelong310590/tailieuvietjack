652f993275df1ee71145c765bc2053aa6da50010
viplct <longlv2710@gmail.com>
2019-05-15 15:11:22 +0700

    view xem truoc

--- src/module/component/doc/Document.js
+++ src/module/component/doc/Document.js
@@ -295,7 +295,8 @@ class Document extends Component {
 									{previewfile!=='' ?
 										(
 											<Fragment>
-												<iframe style={styles} src={"https://docs.google.com/gview?url="+process.env.REACT_APP_BASE_URL+previewfile+"&embedded=true"}></iframe>
+												<embed src={process.env.REACT_APP_BASE_URL+previewfile} width="100%" height="500px" />
+												{/*<iframe style={styles} src={"https://docs.google.com/gview?url="+process.env.REACT_APP_BASE_URL+previewfile+"&embedded=true"}></iframe>*/}
 												<div className="document-middle-ads">
 													<img src="/lib/images/document-ads.jpg" alt="" className="img-responsive"/>
 												</div>
