30d89ecca59523006e38ff741c9d3f7f8d9d41f0
viplct <longlv2710@gmail.com>
2019-05-20 16:57:11 +0700

    view

--- src/module/component/doc/Document.js
+++ src/module/component/doc/Document.js
@@ -300,7 +300,7 @@ class Document extends Component {
 												{formats=='pdf'?(
 													<iframe style={styles} src={process.env.REACT_APP_BASE_URL+previewfile+"#toolbar=0"}></iframe>
 												):(
-													<iframe style={styles} src={"https://docs.google.com/viewer?url="+process.env.REACT_APP_BASE_URL+previewfile+"#toolbar=0"}></iframe>
+													<iframe style={styles} src={"https://docs.google.com/viewer?url="+process.env.REACT_APP_BASE_URL+previewfile+"&embedded=true"}></iframe>
 												)}
 												<div className="document-middle-ads">
 													<img src="/lib/images/document-ads.jpg" alt="" className="img-responsive"/>
